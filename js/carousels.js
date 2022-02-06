20ccbf9dda191156ca572b3db68ed3dbe407f6a5
Jiyeon <yeeoni_@naver.com>
2022-02-06 19:47:36 +0900

    🚧 Initiate a new carousel: .user-gallery

--- js/carousels.js
+++ js/carousels.js
@@ -10,3 +10,21 @@ const productCarousel = tns({
   mouseDrag: true,
   preventScrollOnTouch: true,
 })
+
+const userGallery = tns({
+  container: '.user-gallery .slider-list',
+  controls: false,
+  gutter: 4,
+  edgePadding: 16,
+  loop: false,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+  responsive: {
+    768: {
+      gutter: 6,
+      controls: true,
+      edgePadding: 75,
+    },
+  },
+})
