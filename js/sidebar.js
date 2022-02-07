2dcfe3a50be8dc56bf30ea8a430a8be2ad13d271
Jiyeon <yeeoni_@naver.com>
2022-02-08 00:45:33 +0900

    🚧 Implement toggling sidebar menu

---
+++ js/sidebar.js
@@ -0,0 +1,16 @@
+const sidebarMenuButton = document.querySelector('.gnb-icon-button.is-menu')
+const sidebar = document.querySelector('.sidebar')
+const sidebarOverlay = document.querySelector('.overlay')
+
+function openSidebar() {
+  sidebar.classList.add('is-active')
+  sidebarOverlay.classList.add('is-active')
+}
+
+function closeSidebar() {
+  sidebar.classList.remove('is-active')
+  sidebarOverlay.classList.remove('is-active')
+}
+
+sidebarMenuButton.addEventListener('click', openSidebar)
+sidebarOverlay.addEventListener('click', closeSidebar)
