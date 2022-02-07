63d80ad2d0f2166bb393bf237963f6b1041acacd
Jiyeon <yeeoni_@naver.com>
2022-02-08 02:30:37 +0900

    🚧 Implement toggling search-modal

---
+++ js/search-modal.js
@@ -0,0 +1,19 @@
+const searchModal = document.querySelector('.search-modal')
+const searchOverlay = document.querySelector('.overlay')
+const searchButton = document.querySelector('.gnb-icon-button.is-search')
+const searchModalCloseButton = searchModal.querySelector(
+  '.search-modal-form .btn-ghost'
+)
+
+function openSearchModal() {
+  searchModal.classList.add('is-active')
+  searchOverlay.classList.add('is-active')
+}
+
+function closeSearchModal() {
+  searchModal.classList.remove('is-active')
+  searchOverlay.classList.remove('is-active')
+}
+
+searchButton.addEventListener('click', openSearchModal)
+searchModalCloseButton.addEventListener('click', closeSearchModal)
